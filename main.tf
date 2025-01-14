4d1b9fea439e8a65a48ef9c7c96c651862a74256
aaron lim <aaron.limse@hotmail.com>
2025-01-14 22:41:32 +0800

    ASG (web, db) with ALB to Route 53 deployment. Need to test NLB connectivity with web asg

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ resource "aws_vpc" "aalimsee_tf_main" {
   enable_dns_hostnames = true
 
   tags = {
-    Name      = "aalimsee-tf-vpc"
-    CreatedBy = "aalimsee-tf"
+    Name      = "${var.prefix}-vpc"
+    CreatedBy = "${var.createdByTerraform}"
   }
 }
 
@@ -23,8 +23,8 @@ resource "aws_subnet" "aalimsee_tf_public" {
   availability_zone       = data.aws_availability_zones.available.names[count.index]
 
   tags = {
-    Name      = "aalimsee-tf-public-subnet-${count.index + 1}"
-    CreatedBy = "aalimsee-tf"
+    Name      = "${var.prefix}-public-subnet-${count.index + 1}"
+    CreatedBy = "${var.createdByTerraform}"
   }
 }
 
@@ -35,8 +35,8 @@ resource "aws_subnet" "aalimsee_tf_private" {
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
   tags = {
-    Name      = "aalimsee-tf-private-subnet-${count.index + 1}"
-    CreatedBy = "aalimsee-tf"
+    Name      = "${var.prefix}-private-subnet-${count.index + 1}"
+    CreatedBy = "${var.createdByTerraform}"
   }
 }
 
@@ -44,8 +44,8 @@ resource "aws_internet_gateway" "aalimsee_tf_igw" {
   vpc_id = aws_vpc.aalimsee_tf_main.id
 
   tags = {
-    Name      = "aalimsee-tf-igw"
-    CreatedBy = "aalimsee-tf"
+    Name      = "${var.prefix}-igw"
+    CreatedBy = "${var.createdByTerraform}"
   }
 }
 
@@ -58,8 +58,8 @@ resource "aws_route_table" "aalimsee_tf_public" {
   }
 
   tags = {
-    Name      = "aalimsee-tf-public-route-table"
-    CreatedBy = "aalimsee-tf"
+    Name      = "${var.prefix}-public-route-table"
+    CreatedBy = "${var.createdByTerraform}"
   }
 }
 
@@ -70,7 +70,7 @@ resource "aws_route_table_association" "aalimsee_tf_public" {
 }
 
 resource "aws_security_group" "web" {
-  name   = "aalimsee-tf-web-sg"
+  name   = "${var.prefix}-web-sg"
   vpc_id = aws_vpc.aalimsee_tf_main.id
 
   ingress {
@@ -98,13 +98,13 @@ resource "aws_security_group" "web" {
   }
 
   tags = {
-    Name      = "aalimsee-tf-web-sg"
-    CreatedBy = "aalimsee-tf"
+    Name      = "${var.prefix}-web-sg"
+    CreatedBy = "${var.createdByTerraform}"
   }
 }
 
 resource "aws_security_group" "nlb" {
-  name   = "aalimsee-tf-nlb-sg"
+  name   = "${var.prefix}-nlb-sg"
   vpc_id = aws_vpc.aalimsee_tf_main.id
 
   ingress {
@@ -124,16 +124,18 @@ resource "aws_security_group" "nlb" {
   }
 
   tags = {
-    Name      = "aalimsee-tf-nlb-sg"
-    CreatedBy = "aalimsee-tf"
+    Name      = "${var.prefix}-nlb-sg"
+    CreatedBy = "${var.createdByTerraform}"
   }
 }
 
 resource "aws_launch_template" "web_asg_lt" {
-  name          = "aalimsee-tf-web-launch-template"
+  name          = "${var.prefix}-web-launch-template"
   image_id      = "ami-05576a079321f21f8"
   instance_type = "t2.micro"
-  key_name      = "aalimsee-keypair"
+  key_name      = "${var.key-pair}"
+  update_default_version = true 
+  description   = "${var.createdByTerraform}"
 
   metadata_options {
     http_tokens                 = "optional" # Allows both IMDSv1 and IMDSv2
@@ -142,26 +144,26 @@ resource "aws_launch_template" "web_asg_lt" {
   }
 
   user_data = base64encode(<<-EOF
-#!/bin/bash
-yum update -y
-yum install httpd -y
-echo "<h1>Hello from Application 1, Aaron Lim</h1>" | sudo tee /var/www/html/index.html
-echo "<h1>Hello from Instance $(curl -s http://169.254.169.254/latest/meta-data/instance-id)</h1>" | sudo tee -a /var/www/html/index.html
-systemctl start httpd
-systemctl enable httpd
-EOF
+    #!/bin/bash
+    yum update -y
+    yum install httpd -y
+    echo "<h1>Hello from Application 1, Aaron Lim</h1>" | sudo tee /var/www/html/index.html
+    echo "<h1>Hello from Instance $(curl -s http://169.254.169.254/latest/meta-data/instance-id)</h1>" | sudo tee -a /var/www/html/index.html
+    systemctl start httpd
+    systemctl enable httpd
+    EOF
   )
 
   vpc_security_group_ids = [aws_security_group.web.id]
 
   tags = {
-    Name      = "aalimsee-tf-web-instance"
-    CreatedBy = "aalimsee-tf"
+    Name      = "${var.prefix}-web-instance"
+    CreatedBy = "${var.createdByTerraform}"
   }
 }
 
 resource "aws_autoscaling_group" "web_asg" {
-  name = "aalimsee-tf-web-asg"
+  name = "${var.prefix}-web-asg"
   launch_template {
     id      = aws_launch_template.web_asg_lt.id
     version = "$Latest"
@@ -174,28 +176,30 @@ resource "aws_autoscaling_group" "web_asg" {
 
   tag {
     key                 = "Name"
-    value               = "aalimsee-tf-web-asg"
+    value               = "${var.prefix}-web-asg"
     propagate_at_launch = true
   }
 }
 
 resource "aws_launch_template" "db_asg_lt" {
-  name          = "aalimsee-tf-db-launch-template"
+  name          = "${var.prefix}-db-launch-template"
   image_id      = "ami-05576a079321f21f8"
   instance_type = "t2.micro"
   key_name      = "aalimsee-keypair"
+  update_default_version = true
+  description   = "${var.createdByTerraform}"
 
   tag_specifications {
     resource_type = "instance"
     tags = {
-      Name      = "aalimsee-tf-db-instance"
-      CreatedBy = "aalimsee-tf"
+      Name      = "${var.prefix}-db-instance"
+      CreatedBy = "${var.createdByTerraform}"
     }
   }
 }
 
 resource "aws_autoscaling_group" "db_asg" {
-  name = "aalimsee-tf-db-asg"
+  name = "${var.prefix}-db-asg"
   launch_template {
     id      = aws_launch_template.db_asg_lt.id
     version = "$Latest"
@@ -208,34 +212,72 @@ resource "aws_autoscaling_group" "db_asg" {
 
   tag {
     key                 = "Name"
-    value               = "aalimsee-tf-db-asg"
+    value               = "${var.prefix}-db-asg"
     propagate_at_launch = true
   }
 }
 
 resource "aws_lb" "public_alb" {
-  name               = "aalimsee-tf-alb"
+  name               = "${var.prefix}-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web.id]
   subnets            = aws_subnet.aalimsee_tf_public[*].id
 
   tags = {
-    Name      = "aalimsee-tf-alb"
-    CreatedBy = "aalimsee-tf"
+    Name      = "${var.prefix}-alb"
+    CreatedBy = "${var.createdByTerraform}"
   }
 }
 
 resource "aws_lb_target_group" "public_tg" {
-  name     = "aalimsee-tf-alb-target-group"
+  name     = "${var.prefix}-alb-target-group"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.aalimsee_tf_main.id
 
-  tags = {
-    Name      = "aalimsee-tf-alb-target-group"
-    CreatedBy = "aalimsee-tf"
+  health_check {
+    interval            = 30
+    timeout             = 5
+    protocol            = "HTTP"
+    path                = "/"
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
   }
+
+  tags = {
+    Name      = "${var.prefix}-alb-target-group"
+    CreatedBy = "${var.createdByTerraform}"
+  }
+}
+
+
+#==========================================================
+# Fetching Instance IDs from an Auto Scaling Group
+#==========================================================
+data "aws_autoscaling_group" "web_asg" {
+  name = aws_autoscaling_group.web_asg.name
+}
+
+data "aws_instances" "asg_instances" {
+  filter {
+    name   = "tag:aws:autoscaling:groupName"
+    values = [data.aws_autoscaling_group.web_asg.name]
+  }
+}
+
+output "asg_instance_ids" {
+  value = data.aws_instances.asg_instances.ids
+}
+
+#==========================================================
+# Registering Instances to an ALB with ASG
+#==========================================================
+resource "aws_lb_target_group_attachment" "targets" {
+  count            = length(data.aws_instances.asg_instances.ids)
+  target_group_arn = aws_lb_target_group.public_tg.arn
+  target_id        = data.aws_instances.asg_instances.ids[count.index]
+  port             = 80
 }
 
 resource "aws_lb_listener" "public_listener" {
@@ -250,26 +292,26 @@ resource "aws_lb_listener" "public_listener" {
 }
 
 resource "aws_lb" "internal_nlb" {
-  name               = "aalimsee-tf-nlb"
+  name               = "${var.prefix}-nlb"
   internal           = true
   load_balancer_type = "network"
   subnets            = aws_subnet.aalimsee_tf_private[*].id
 
   tags = {
-    Name      = "aalimsee-tf-nlb"
-    CreatedBy = "aalimsee-tf"
+    Name      = "${var.prefix}-nlb"
+    CreatedBy = "${var.createdByTerraform}"
   }
 }
 
 resource "aws_lb_target_group" "internal_tg" {
-  name     = "aalimsee-tf-nlb-target-group"
+  name     = "${var.prefix}-nlb-target-group"
   port     = 3306
   protocol = "TCP"
   vpc_id   = aws_vpc.aalimsee_tf_main.id
 
   tags = {
-    Name      = "aalimsee-tf-nlb-target-group"
-    CreatedBy = "aalimsee-tf"
+    Name      = "${var.prefix}-nlb-target-group"
+    CreatedBy = "${var.createdByTerraform}"
   }
 }
 
@@ -283,3 +325,22 @@ resource "aws_lb_listener" "internal_listener" {
     target_group_arn = aws_lb_target_group.internal_tg.arn
   }
 }
+
+#==========================================================
+# Reference the existing Route 53 Hosted Zone
+data "aws_route53_zone" "existing_zone" {
+  name = "sctp-sandbox.com" # Replace with your exact domain
+}
+
+# Route 53 Record for Web Load Balancer
+resource "aws_route53_record" "public_alb" {
+  zone_id = data.aws_route53_zone.existing_zone.id
+  name    = "aalimsee-tf-web" # Subdomain for the web service
+  type    = "A"
+  alias {
+    name                   = aws_lb.public_alb.dns_name
+    zone_id                = aws_lb.public_alb.zone_id
+    evaluate_target_health = true
+  }
+}
+#==========================================================
---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  type = string
+  default = "aalimsee-tf"
+}
+
+variable "createdByTerraform" {
+  type = string
+  default = "Managed by Terraform - Aaron"
+}
+
+variable "key-pair" {
+  type = string
+  default = "aalimsee-keypair"
+}
