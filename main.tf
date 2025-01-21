79679f4fcd6cb36e08b773bc12f5c37ab1b829ab
aaron lim <aaron.limse@hotmail.com>
2025-01-21 11:41:05 +0800

    user data http added to db instances

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_launch_template" "web_asg_lt" {
     #!/bin/bash
     yum update -y
     yum install httpd -y
-    echo "<h1>Hello from Application 1, Aaron Lim</h1>" | sudo tee /var/www/html/index.html
+    echo "<h1>Hello from Application ASG 1, Aaron Lim</h1>" | sudo tee /var/www/html/index.html
     echo "<h1>Hello from Instance $(curl -s http://169.254.169.254/latest/meta-data/instance-id)</h1>" | sudo tee -a /var/www/html/index.html
     systemctl start httpd
     systemctl enable httpd
@@ -70,21 +70,35 @@ resource "aws_launch_template" "db_asg_lt" {
   update_default_version = true
   description   = "${var.createdByTerraform}"
 
-    # <<< add this block
-    user_data = base64encode(<<-EOF
-      #!/bin/bash
-      # Update system
-      sudo yum update -y
-      # Install Squid proxy
-      sudo yum install squid -y
-      # Configure Squid to allow traffic from the VPC CIDR
-      sudo echo "acl allowed_network src 10.0.0.0/16" >> /etc/squid/squid.conf
-      sudo echo "http_access allow allowed_network" >> /etc/squid/squid.conf
-      # Restart Squid to apply changes
-      sudo systemctl enable squid
-      sudo systemctl restart squid
-      EOF
-    )
+  metadata_options {
+    http_tokens                 = "optional" # Allows both IMDSv1 and IMDSv2
+    http_endpoint               = "enabled"  # Enables access to the metadata service
+    http_put_response_hop_limit = 4          # Optional, sets the hop limit for metadata requests
+  }
+  # <<< add this block
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    # Update system
+    sudo yum update -y
+    
+    # Install Squid proxy
+    sudo yum install squid -y
+    # Configure Squid to allow traffic from the VPC CIDR
+    sudo echo "acl allowed_network src 10.0.0.0/16" >> /etc/squid/squid.conf
+    sudo echo "http_access allow allowed_network" >> /etc/squid/squid.conf
+    # Restart Squid to apply changes
+    sudo systemctl enable squid
+    sudo systemctl restart squid
+
+    # Install HTTP services
+    yum install httpd -y
+    echo "<h1>Hello from Database ASG 1, Aaron Lim</h1>" | sudo tee /var/www/html/index.html
+    echo "<h1>Hello from Instance $(curl -s http://169.254.169.254/latest/meta-data/instance-id)</h1>" | sudo tee -a /var/www/html/index.html
+    systemctl start httpd
+    systemctl enable httpd
+    
+    EOF
+  )
 
   vpc_security_group_ids = [aws_security_group.nlb.id]
 
